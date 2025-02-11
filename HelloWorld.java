c8813c12c8a021a9eb4a612a9d348f7b68ab4bc8
Dev Mistry <dmistry6487@gmail.com>
2025-02-11 11:36:23 +0530

    helloworldAddes2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
     public static void main(String[] args){
         for(int i=0;i<10;i++){
-            System.out.println("Inside main method");
+            System.out.println("Inside main method" + i );
         }
     }
 }
